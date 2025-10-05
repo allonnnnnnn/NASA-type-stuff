0c5551e2939f900eff669c826d7a98768bb054bb
Seonuk Baek <sbaek19@my.bcit.ca>
2025-10-05 14:33:34 -0700

    added .env file on the frontned

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,19 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase config from .env
 const firebaseConfig = {
-  apiKey: "AIzaSyCWgK6l253kKhdqCHswTQ5b0vfwZsN6_Rs",
-  authDomain: "nasahackathon-f33a9.firebaseapp.com",
-  projectId: "nasahackathon-f33a9",
-  storageBucket: "nasahackathon-f33a9.firebasestorage.app",
-  messagingSenderId: "111808160443",
-  appId: "1:111808160443:web:f578d99d95e40fcd35ff9e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export commonly used services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
